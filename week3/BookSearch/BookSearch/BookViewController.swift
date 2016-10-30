6cab43794d6a81ebe1e3546721f85b97b06d847a
Guillermo Varela <guille.varela3.16@gmail.com>
2016-10-30 09:26:38 +0000

    Improvements

--- week3/BookSearch/BookSearch/BookViewController.swift
+++ week3/BookSearch/BookSearch/BookViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class BookViewController: UIViewController {
 
-    var book: Book!
+    var currentBook: Book!
+    var booksHolderDelegate: BooksHolderProtocol!
+    var dataTask: URLSessionDataTask?
+    let openLibraryEndpointComponents = NSURLComponents()
 
+    @IBOutlet weak var searchLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var isbnLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var authorsLabel: UILabel!
     @IBOutlet weak var noCoverLabel: UILabel!
@@ -21,16 +28,23 @@ class BookViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        openLibraryEndpointComponents.scheme = "https";
+        openLibraryEndpointComponents.host = "openlibrary.org";
+        openLibraryEndpointComponents.path = "/api/books";
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        titleLabel.text = book.title
-        authorsLabel.text = book.authors?.joined(separator: ", ")
-        if let coverUrl = book.coverUrl {
-            downloadCover(coverUrl)
-            noCoverLabel.isHidden = true
+        if currentBook != nil {
+            showBook(currentBook, true)
         } else {
-            noCoverLabel.isHidden = false
+            searchLabel.isHidden = false
+            searchTextField.isHidden = false
+            resultsLabel.isHidden = false
+            isbnLabel.text = nil
+            titleLabel.text = nil
+            authorsLabel.text = nil
+            coverUIImageView.image = nil
+            noCoverLabel.isHidden = true
         }
     }
 
@@ -38,7 +52,102 @@ class BookViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func searchBook(_ sender: UITextField) {
+        if let isbn = sender.text {
+            if isbn.isEmpty {
+                showError("ISBN inválido.")
+            } else {
+                findBook(isbn)
+            }
+        } else {
+            showError("ISBN inválido.")
+        }
+    }
+
+    private func findBook(_ isbn: String) {
+        if dataTask != nil && dataTask?.state == .running {
+            return
+        }
+        let jscmdQuery = URLQueryItem(name: "jscmd", value: "data")
+        let formatQuery = URLQueryItem(name: "format", value: "json")
+        let bibkeysQuery = URLQueryItem(name: "bibkeys", value: "ISBN:" + isbn)
+        openLibraryEndpointComponents.queryItems = [jscmdQuery, formatQuery, bibkeysQuery]
+        
+        let urlRequest = URLRequest(url: openLibraryEndpointComponents.url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30.0)
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        dataTask = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                // check for errors
+                guard error == nil else {
+                    if error?.localizedDescription != "cancelled" {
+                        self.showError("Error consultando, por favor verifica tu conexión a Internet.")
+                    }
+                    return
+                }
+                // check for data
+                if let data = data {
+                    do {
+                        guard let book = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {
+                            self.showError("Error procesando la respuesta del servidor.")
+                            return
+                        }
+                        if book.count == 0 {
+                            self.showError("No se encontró un libro con ese ISBN.")
+                        } else {
+                            self.currentBook = self.buildBook(book, isbn)
+                            self.showBook(self.currentBook, false)
+                            self.searchTextField.text = nil
+                            self.booksHolderDelegate.addBook(self.currentBook)
+                        }
+                    } catch  {
+                        self.showError("Error procesando la respuesta del servidor.")
+                    }
+                } else {
+                    self.showError("No se obtuvo una respuesta del servidor, por favor intenta de nuevo más tarde.")
+                }
+            }
+        })
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        dataTask?.resume()
+    }
     
+    private func buildBook(_ book : [String : AnyObject]?, _ isbn : String) -> Book {
+        var bookObject = Book()
+        bookObject.isbn = isbn
+        if let bookContent = book?["ISBN:" + isbn] as! [String : AnyObject]? {
+            bookObject.title = bookContent["title"] as? String
+            if let authors = bookContent["authors"] as! [[String : String]]? {
+                bookObject.authors = authors.map({$0["name"]!})
+            }
+            if let cover = bookContent["cover"] as! [String : AnyObject]? , let coverUrl = cover["medium"] as! String? {
+                bookObject.coverUrl = URL(string: coverUrl)!
+            }
+        }
+        return bookObject
+    }
+
+    private func showBook(_ book: Book, _ hideSearch: Bool) -> Void {
+        if hideSearch {
+            searchLabel.isHidden = true
+            searchTextField.isHidden = true
+            resultsLabel.isHidden = true
+        }
+        isbnLabel.text = book.isbn
+        titleLabel.text = book.title
+        authorsLabel.text = book.authors?.joined(separator: ", ")
+        if let coverUrl = book.coverUrl {
+            downloadCover(coverUrl)
+            noCoverLabel.isHidden = true
+        } else {
+            noCoverLabel.isHidden = false
+            self.coverUIImageView.image = nil
+        }
+    }
+
     private func downloadCover(_ url : URL) -> Void {
         let urlRequest = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30.0)
         let session = URLSession(configuration: URLSessionConfiguration.default)
@@ -49,6 +158,7 @@ class BookViewController: UIViewController {
                 // check for errors
                 guard error == nil else {
                     self.showError("Error descargando la portada, por favor verifica tu conexión a Internet.")
+                    self.coverUIImageView.image = nil
                     return
                 }
                 // check for data
@@ -65,15 +175,4 @@ class BookViewController: UIViewController {
         alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in}))
         self.present(alertController, animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- week3/BookSearch/BookSearch/SearchTableViewController.swift
+++ week3/BookSearch/BookSearch/SearchTableViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class SearchTableViewController: UITableViewController {
+protocol BooksHolderProtocol {
+    func addBook(_ book: Book)
+}
+
+class SearchTableViewController: UITableViewController, BooksHolderProtocol {
 
     let reuseIdentifier = "reuseIdentifier"
-    let openLibraryEndpointComponents = NSURLComponents()
     var books = [Book]()
-    var dataTask: URLSessionDataTask?
 
     @IBOutlet weak var isbnTextField: UITextField!
 
@@ -25,10 +27,10 @@ class SearchTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
 
-        openLibraryEndpointComponents.scheme = "https";
-        openLibraryEndpointComponents.host = "openlibrary.org";
-        openLibraryEndpointComponents.path = "/api/books";
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,92 +42,11 @@ class SearchTableViewController: UITableViewController {
         return "Libros Encontrados"
     }
 
-    @IBAction func searchButtonClicked(_ sender: UIBarButtonItem) {
-        if let isbn = isbnTextField.text {
-            if isbn.isEmpty {
-                showError("ISBN inválido.")
-            } else {
-                findBook(isbn)
-            }
-        } else {
-            showError("ISBN inválido.")
+    // MARK: - Books Holder Protocol
+    func addBook(_ book: Book) {
+        if !self.books.contains(book) {
+            self.books.append(book)
         }
-    }
-
-    private func showError(_ message : String) -> Void {
-        let alertController = UIAlertController(title: "Ups", message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in}))
-        self.present(alertController, animated: true, completion: nil)
-    }
-
-    private func findBook(_ isbn: String) {
-        if dataTask != nil && dataTask?.state == .running {
-            return
-        }
-        let jscmdQuery = URLQueryItem(name: "jscmd", value: "data")
-        let formatQuery = URLQueryItem(name: "format", value: "json")
-        let bibkeysQuery = URLQueryItem(name: "bibkeys", value: "ISBN:" + isbn)
-        openLibraryEndpointComponents.queryItems = [jscmdQuery, formatQuery, bibkeysQuery]
-
-        let urlRequest = URLRequest(url: openLibraryEndpointComponents.url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30.0)
-        let session = URLSession(configuration: URLSessionConfiguration.default)
-
-        dataTask = session.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
-            DispatchQueue.main.async {
-                UIApplication.shared.isNetworkActivityIndicatorVisible = false
-
-                // check for errors
-                guard error == nil else {
-                    if error?.localizedDescription != "cancelled" {
-                        self.showError("Error consultando, por favor verifica tu conexión a Internet.")
-                    }
-                    return
-                }
-                // check for data
-                if let data = data {
-                    do {
-                        guard let book = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {
-                            self.showError("Error procesando la respuesta del servidor.")
-                            return
-                        }
-                        if book.count == 0 {
-                            self.showError("No se encontró un libro con ese ISBN.")
-                        } else {
-                            let bookFound = self.buildBook(book, isbn)
-                            if self.books.contains(bookFound) {
-                                self.showError("Búsqueda repetida.")
-                            } else {
-                                self.books.append(bookFound)
-                                self.tableView.reloadData()
-                                self.isbnTextField.text = nil
-                                self.performSegue(withIdentifier: "bookDetails", sender: bookFound)
-                            }
-                        }
-                    } catch  {
-                        self.showError("Error procesando la respuesta del servidor.")
-                    }
-                } else {
-                    self.showError("No se obtuvo una respuesta del servidor, por favor intenta de nuevo más tarde.")
-                }
-            }
-        })
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-        dataTask?.resume()
-    }
-
-    private func buildBook(_ book : [String : AnyObject]?, _ isbn : String) -> Book {
-        var bookObject = Book()
-        bookObject.isbn = isbn
-        if let bookContent = book?["ISBN:" + isbn] as! [String : AnyObject]? {
-            bookObject.title = bookContent["title"] as? String
-            if let authors = bookContent["authors"] as! [[String : String]]? {
-                bookObject.authors = authors.map({$0["name"]!})
-            }
-            if let cover = bookContent["cover"] as! [String : AnyObject]? , let coverUrl = cover["medium"] as! String? {
-                bookObject.coverUrl = URL(string: coverUrl)!
-            }
-        }
-        return bookObject
     }
 
     // MARK: - Table view data source
@@ -142,6 +63,10 @@ class SearchTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         cell.textLabel?.text = books[indexPath.item].title
         return cell
+    }
+
+    @IBAction func addButtonClicked(_ sender: UIBarButtonItem) {
+        self.performSegue(withIdentifier: "bookDetails", sender: sender)
     }
 
     /*
@@ -186,10 +111,9 @@ class SearchTableViewController: UITableViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationViewController = segue.destination as! BookViewController
-        if let book = sender as? Book {
-            destinationViewController.book = book
-        } else if let selectedRow = tableView.indexPathForSelectedRow {
-            destinationViewController.book = books[selectedRow.item]
+        destinationViewController.booksHolderDelegate = self
+        if let selectedRow = tableView.indexPathForSelectedRow {
+            destinationViewController.currentBook = books[selectedRow.item]
         }
     }
 }
